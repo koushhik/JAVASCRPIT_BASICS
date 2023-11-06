b795c92899bec2a5f2fcfe4f1003ac044e398ac8
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-06 12:28:40 +0530

    7) script_tag_in_head_section.js
    7) script_tag_in_head_section.js
    
    to change the paragraph from original to new using JavaScript tag <script> at the head section of HTML document.
    
    <!--IMPORTANT TAKEAWAYS:
        
      HERE YOU DONT NEED TO DECLIRE SCRIPT TAG AS IT IS A BASIC FUNCTION OF HTML. AND EXTENSION REMAINS SAME (anyfilename.html)
    
     BUT I WILL SAVE IT AS JS EXTENSION FOR GIT HUB REASONS.. ...YOU PLZ CHANGE TO .html FORMAT WHICH IS PROPER.
    
     1)WE WRITE A SCRIPT TAG AT THE HEAD SECTION
    
     
     2)CREATE A FUNCTION BY:
       
         SYNTAX: 
         function function_name()
         {
            document.getElementById("id_name").innerHTML="MESSAGE";
         }
    
    
     3) IN BODY SECTION: we then create an ID for PARAGRAPH:
    
        EXAMPLE : <p id="id_name">THIS IS A PARAGRAPH</p>
    
    
     4) IN BODY SECTION : WE CREATE BUTTON TO BE CLICKED AND TO ACCESS THE FUNCTION IN SCRIPT TAG.
        
        SYNTAX:  <button type="button" onclick="function_name()">CLICK THIS BUTTON</button>
    
    
    -->

---
+++ 7) script_tag_in_head_section.js
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+       <title>NEW JAVASCRIPT</title>
+    </head>
+
+    <script>
+        function myFun()
+        {
+            document.getElementById("head_tag").innerHTML="changing of paragrpah";
+        }
+    </script>
+
+    <body>
+
+
+        <p id="head_tag">this is paragraph that needs to be changed</p>
+        <button type="button" onclick="myFun()"> CLICK THIS </button>
+        
+    
+         
+    </body>
+
+</html>
