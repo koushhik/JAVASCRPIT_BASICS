973cf9771d2e172be457b4745fb26ba8e8d3b735
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-06 15:16:06 +0530

    14) variable_using_let_in_single_line.js
    14) variable_using_let_in_single_line.js
    
    write a script to add two numbers and store them in a single line using the let keyword. using semicolon (;).
    
        Change file name from .js to .html format
        
        -> CREATE A BASIC ATTIBUTES WITH H1 , P
        -> CREATE A SCRIPT INSIDE IN WHICH CREATE 3 VARIABLES...x,y,z with 'let' keyword
        -> add two variables and store it the third variable in a single line followed by a semicolon (;).
        -> using 'get element by id' with 'inner html' display the total result.

---
+++ 14) variable_using_let_in_single_line.js
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+       <title>NEW JAVASCRIPT</title>
+    </head>
+
+    
+
+    <body>
+
+   <h1>DISPLAY USING ALERT FUNCTION</h1>
+   <P>ALERT METHOD IS USED TO DISPLAY SOME IMPORTANT CAUTION OR MESSGAES ON TOP OF THE PAGE AS A NOTIFICATION PANEL.</P>
+ <p id="addition"></p>
+ <script>
+    let a = 10;b = 25;c = a + b;
+    document.getElementById('addition').innerHTML="THE TOTAL SUM IS : " + c + ".";
+ </script>
+         
+    </body>
+
+</html>
+
