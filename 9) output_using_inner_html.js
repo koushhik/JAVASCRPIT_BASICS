ff7dbdfedc4c9117a693dfbc7f5abc6f65e99136
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-06 14:14:50 +0530

    9) output_using_inner_html.js
    9) output_using_inner_html.js:
    
    displaying addition of 20 + 4 as addition to show the inner HTML method for output.
    
      Change file name from .js to .html format
        
        -> CREATE A BASIC ATTRIBUTES WITH H1 , P
        -> CREATE AN EMPTY PARAGRAPH ADD AN ID TO IT ,TO DISPLAY JS SCRIPT
        -> IN SCRIPT USE GET ELEMENT BY ID METHOD AND INNER- HTML TO DISPLAY THE MESSAGE.

---
+++ 9) output_using_inner_html.js
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+       <title>NEW JAVASCRIPT</title>
+    </head>
+
+    
+
+    <body>
+
+        <h1>DISPLAYING USING INNER HTML</h1>
+        <P>The following paragraph will show the addition of two numbers :</P>
+        <p id="remo">  </p>
+
+        <script>
+            document.getElementById("remo").innerHTML= 20 + 4;
+        </script>
+
+    
+         
+    </body>
+
+</html>
