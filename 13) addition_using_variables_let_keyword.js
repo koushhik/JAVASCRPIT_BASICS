69085068daf26d9448aeb3ffa5d77651a890ab47
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-06 15:07:25 +0530

    13) addition_using_variables_let_keyword.js
    13) addition_using_variables_let_keyword.js
    
    add two variable and store it in third variable using the let keyword.
    
        Change file name from .js to .html format
        
        -> CREATE A BASIC ATTRIBUTES WITH H1 , P
        -> CREATE A SCRIPT INSIDE IN WHICH CREATE 3 VARIABLES...x,y,z with 'let' keyword
        -> add two variables and store it the third variable.
        -> using 'get element by id' with 'inner html' display the total result.

---
+++ 13) addition_using_variables_let_keyword.js
@@ -0,0 +1,30 @@
+
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+       <title>NEW JAVASCRIPT</title>
+    </head>
+
+    
+
+    <body>
+
+   <h1>DISPLAY USING ALERT FUNCTION</h1>
+   <P>ALERT METHOD IS USED TO DISPLAY SOME IMPORTANT CAUTION OR MESSGAES ON TOP OF THE PAGE AS A NOTIFICATION PANEL.</P>
+ <p id="addition"></p>
+ <script>
+    let x,y,z;
+    x = 10;
+    y = 20;
+    z = x + y;
+    document.getElementById('addition').innerHTML="THE TOTAL SUM IS : " + z + ".";
+ </script>
+         
+    </body>
+
+</html>
+
+
+
+    
