118829adcc0704dbdfd2ba7ba4415f6a54e90f0a
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-06 14:23:14 +0530

    10) output_using_write_function.js
    10) output_using_write_function.js
    
    display the output using the write() , but note that write function isn't often used as it has risk of loosing / rewriting of original data while loading .
    
    Change file name from .js to .html format
        
        -> CREATE A BASIC ATTIBUTES WITH H1 , P
        -> THEN ON IN SCRIPT TAG WRITE THE WRITE FUNTION AS FOLLOWS:
        -> EXAMPLE: document.write(message);
        -> display the output in browser.

---
+++ 10) output_using_write_function.js
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+       <title>NEW JAVASCRIPT</title>
+    </head>
+
+    
+
+    <body>
+
+   <h1>USING THE WRITE() METHOD</h1>
+   <p>this is a pargraph</p>
+   <p>its not recommended for most cases as loding of html will result in overwriting of original content.</p>
+
+   <script>
+    document.write(20 + 5);
+   </script>
+         
+    </body>
+
+</html>
