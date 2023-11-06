ce0e49ea270554f83180611467165da1e3221147
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-06 12:57:44 +0530

    8) script_at_body_section.js
    8) script_at_body_section.js
    
    create document to change paragraph by using script tag at the body of HTML document.
    
        Change file name from .js to .html format
        
        1)here we create <script> tag inside the body 
        2)we write a function called headFun()
        3)inside the function we access the id of paragrpah by:
           document.getElementById("id_name")
    
         4) we display changed content by :
         .innerHTML = "message";
    
         5)we declaire id at paragrpag by : id="id_name"
    
         6)we create a button for clicking purpose and acessing function by :
         <button type="button" onclick="function_name()">CLICK ME</button>

---
+++ 8) script_at_body_section.js
@@ -0,0 +1,31 @@
+
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+       <title>NEW JAVASCRIPT</title>
+    </head>
+
+    
+
+    <body>
+
+        <script>
+
+          function bodyFun()
+          {
+            document.getElementById("body_tag").innerHTML = "this is a new paragraph";
+          }
+
+        </script>
+
+
+        <p id="body_tag">this is paragraph that needs to be changed</p>
+        <button type="button" onclick="bodyFun()"> CLICK THIS </button>
+        
+    
+         
+    </body>
+
+</html>
+
