1835c873fcb393a4df5b362649e3f62436f562c2
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-06 14:34:34 +0530

    11) output_using_alert_in_alert_window.js
    11) output_using_alert_in_alert_window.js
    
    using alert output function display a small message in alert window.
        
        Change file name from .js to .html format
        
        -> CREATE A BASIC ATTIBUTES WITH H1 , P
        -> THEN ON IN SCRIPT TAG WRITE THE WRITE FUNTION AS FOLLOWS:
        -> window.alert("some message")
        -> here window refers to the main panel where you want to display .
        -> in case if you want to display in the web page you use : document.
        -> in case you want to show at the top panel you use the alert box.

---
+++ 11) output_using_alert_in_alert_window.js
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+       <title>NEW JAVASCRIPT</title>
+    </head>
+
+    
+
+    <body>
+
+   <h1>DISPLAY USING ALERT FUNCTION</h1>
+   <P>ALERT METHOD IS USED TO DISPLAY SOME IMPORTANT CAUTION OR MESSGAES ON TOP OF THE PAGE AS A NOTIFICATION PANEL.</P>
+   <script>
+    window.alert("hello there...!!");
+   </script>
+         
+    </body>
+
+</html>
