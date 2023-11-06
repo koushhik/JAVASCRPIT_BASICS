a187345ccdb5e8c599bd0fdad69cf56dce435d49
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-06 11:58:39 +0530

    6) using_script_tag.js
    6) using_script_tag.js
    
    creating a paragraph using script tag. 
    
    
     HERE YOU DONT NEED TO DECLIRE SCRIPT TAG AS IT IS A BASIC FUNCTION OF HTML. AND EXTENSION REMAINS SAME (anyfilename.html)
    
     BUT I WILL SAVE IT AS JS EXTENSION FOR GIT HUB REASONS.. ...YOU PLZ CHANGE TO .html FORMAT WHICH IS PROPER.
    
     1)WE WRITE A SCRIPT INSIDE THE BODY TAG
     2)WE DECLAIRE ID FOR PARAGRPH
     3) WE USE document.getElementById('id_name') to access the id of paragraph.
     4)we continue with .innerHTML = "message to be displayed"  for displaying message at the paragraph position.
     5)we end it with se4micolon (;).

---
+++ 6) using_script_tag.js
@@ -0,0 +1,26 @@
+
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+       <title>NEW JAVASCRIPT</title>
+    </head>
+
+    <body>
+
+        <h1> USING SCRIPT TAG</h1>
+
+        <p id="script_tag"> : </p>
+        
+    <script>
+        document.getElementById('script_tag').innerHTML="this is done by using scripting tag inside html doccument";
+        
+    </script>
+         
+    </body>
+
+</html>
+
+
+
+
